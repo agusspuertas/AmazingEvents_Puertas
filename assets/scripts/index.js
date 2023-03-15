bbb87cb89d9b6eb406324f0d6a0f1105b81f21f3
agusspuertas <agustinnpuertas@gmail.com>
2023-03-15 03:57:42 -0300

    error correction task2

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,33 +1,30 @@
+
 const container = document.getElementById('card-container');
 
 function showData(containerData) {
+  const fragment = document.createDocumentFragment();
+
   for (eventData of data.events) {
     const card = document.createElement('div');
     card.classList.add('card');
     card.innerHTML = `
-       
-        <img src="${eventData.image}" class="card-img-top" alt="Cinema">
-        <div class="title-container d-flex justify-content-around">
-                    <h5 class="card-title">${eventData.category}</h5>
-                    <p class="colorText">${eventData.place}</p>
-                </div>
-        <div class="card-body">
-          <h2 class="card-title-main text-dark fw-bold">${eventData.name}</h2>
-          <p class="card-text text-darck">${eventData.description}</p>
-
-          <div class="card-data">
-          <p>Capacity: ${eventData.capacity}</p>
+      <img src="${eventData.image}" class="card-img-top" alt="Cinema">
+      <div class="card-body">
+        <h2 class="card-title-main text-dark fw-bold">${eventData.name}</h2>
+        <p class="card-text text-darck">${eventData.description}</p>
+        <div class="card-data">
           <p class="colorText">Date: ${eventData.date}</p>
-      </div>
+        </div>
+        <div class="footer-card">
+          <span class="text-secondary fw-bold text-dark">Price $${eventData.price}</span>
+          <a href="./details.html" class="btn btn-secondary ms-auto">Details</a>
+        </div>
+      </div>`;
 
-          <div class="footer-card">
-            <span class="text-secondary fw-bold text-dark">Price $${eventData.price}</span>
-            <a href="./details.html" class="btn btn-secondary ms-auto">Details</a>
-          </div>
-        </div>`
-
-    containerData.append(card);
+    fragment.appendChild(card);
   }
+
+  containerData.appendChild(fragment);
 }
 
 showData(container);
--- assets/scripts/upcoming_events.js
+++ assets/scripts/upcoming_events.js
@@ -3,22 +3,19 @@ const upcomingContainer = document.getElementById('upcoming-container');
 let dateFilter = eventFilter(data.events, data.currentDate);
 
 function showData(containerData) {
+    const fragment = document.createDocumentFragment();
+
     for (eventData of dateFilter) {
         const card = document.createElement('div');
         card.classList.add('card');
         card.innerHTML = `
         
         <img src="${eventData.image}" class="card-img-top" alt="Cinema">
-        <div class="title-container d-flex justify-content-around">
-                    <h5 class="card-title">${eventData.category}</h5>
-                    <p class="colorText">${eventData.place}</p>
-                </div>
         <div class="card-body">
           <h2 class="card-title-main text-dark fw-bold">${eventData.name}</h2>
           <p class="card-text text-darck">${eventData.description}</p>
 
           <div class="card-data">
-          <p>Capacity: ${eventData.capacity}</p>
           <p class="colorText">Date: ${eventData.date}</p>
       </div>
 
@@ -28,9 +25,10 @@ function showData(containerData) {
           </div>
         </div>`
 
-        containerData.append(card);
-    };
-};
+        fragment.appendChild(card);
+    }
+    upcomingContainer.appendChild(fragment);
+}
 
 function eventFilter(events, currentDate) {
     let dataFilter = []
